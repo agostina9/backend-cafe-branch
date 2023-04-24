1e315778ba4587666eb8af96ae5cabf0efb03d68
Agostina <lozagostina@gmail.com>
2023-04-24 15:07:49 -0500

    configuramos un puerto para nuestro backemd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hola mundo');
+import express from "express";
+
+//crear una instalcia de express
+const app = express();
+
+//crear una variable
+app.set("port", process.env.PORT || 4001);
+//usar el puerto
+app.listen( app.get('port'),()=>{
+    console.log('Estoy en el puerto'+ app.get('port'))
+} )
+
+console.log("hola mundooo");
