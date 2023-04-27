cb9f513bc85654466ce20c8d1b539cab9acd373d
Agostina <lozagostina@gmail.com>
2023-04-26 19:59:18 -0500

    agregamos validaciones en POST

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,19 +1,55 @@
 import { Router } from "express";
-import { borrarProducto, crearProducto, editarProducto, listarProductos, obtenerProducto } from "../controllers/productos.controllers";
+import {
+  borrarProducto,
+  crearProducto,
+  editarProducto,
+  listarProductos,
+  obtenerProducto,
+} from "../controllers/productos.controllers";
+import { check } from "express-validator";
 
 const router = Router();
 
 router
   .route("/productos")
-  .get(listarProductos)
-  .post(crearProducto);
+  .get(listarProductos )
+  .post(
+    [
+      check("nombreProducto")
+        .notEmpty()
+        .withMessage("El nombre del producto es un dato obligatorio")
+        .isLength({min:2, max:50})
+        .withMessage('El nombre del producto debe tener entre 2 y 50 caracteres'),
+        check('precio')
+        .notEmpty()
+        .withMessage('El dato es un dato obligatorio')
+        .isNumeric()
+        .withMessage('El precio debe ser un numero')
+        .custom((value)=>{
+          if(value>=1 && value <=10000){
+            return true;
+          }else{
+            throw new Error('El precio debe estar entre 1 y 10000')
+          }
+        }),
+        check('imagen')
+        .matches(/^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/)
+        .withMessage('Debe enviar una URL valida'),
+        check('categoria')
+        .notEmpty()
+        .isIn(['bebida caliente', 'bebida fria','dulce', 'salado'])
+        .withMessage('La categoria debe ser correcta')
+    ],
+    crearProducto
+  );
 
-  router.route('/productos/:id')
+router
+  .route("/productos/:id")
   .get(obtenerProducto)
   .put(editarProducto)
-  .delete(borrarProducto)
+  .delete(borrarProducto);
 
-  export default router
+export default router;
 
 // app.get("/productos", (req, res) => {
 //     res.send("aqui tengo q retornar un arreglo de productos");
